e3cfa2a82b1e389f3b041f9bc5d5af7301c2e239
luis.belmonte <luis.belmonte@biotech.cloud>
2020-05-06 11:08:13 +0800

    added docker files

--- nuxt.config.js
+++ nuxt.config.js
@@ -53,8 +53,8 @@ module.exports = {
     extend(config, ctx) {}
   },
   axios: {
-    baseURL: "http://172.16.4.168:9100"
+    baseURL: "http://94.237.66.240:8000"
   },
 
-  server: { port: 4000}
+  server: { port: 7000}
 };
