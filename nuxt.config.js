f6b923535a914085fc0440d9d847d3fffff6c7b0
Kharyl Kate Ortiz <kharyl.kate.ortiz@biotechfarms.net>
2021-04-12 10:18:31 +0800

    update env

--- nuxt.config.js
+++ nuxt.config.js
@@ -72,8 +72,8 @@ module.exports = {
     // serverPrintUrl: "http://localhost:3002",
     // networkPrinterIp: `172.16.4.173`, // gensan network printer ip
     uuid: `8204064d-1029-4398-98c4-075fe28643b4`,
-    bfi: `4400`,
-    rci: `4381`
+    bfi: `139483`,
+    rci: `139492`
   },
 
   server: { port: 9000 }
