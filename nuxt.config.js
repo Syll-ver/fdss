eb5c50fdfeb846cb387d0c36f8a6e0f48f2e79df
Butch Lendio <butch.lendio@biotechfarms.net>
2021-04-28 12:18:34 +0800

    push

--- nuxt.config.js
+++ nuxt.config.js
@@ -64,7 +64,7 @@ module.exports = {
     // baseURL: "https://fdss-uat.biotechfarms.net/fdss-api" 
     // baseURL: "https://fdss-eut.biotechfarms.net/fdss-api" 
     // baseURL: "http://172.16.4.201:8000/fdss-api" // butch 
-    baseURL: "hhtp://94.237.66.240:8223" //(UAT)
+    baseURL: "http://94.237.66.240:8223" //(UAT)
     // baseURL: "https://fdss-sqa.biotechfarms.net/fdss-api" 
 
   },
